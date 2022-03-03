e7c8e5462d82b5a2345b271239219ea8e83ca502
Soybean <honghuangdc@gmail.com>
2022-03-04 02:31:55 +0800

    build(projects): add vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,52 @@
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 import windicss from 'vite-plugin-windicss';
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  plugins: [
-    vue(),
-    windicss(),
-    dts({
-      include: ['./src/index.ts', './src/index.vue'],
-      beforeWriteFile(filePath, content) {
-        return {
-          filePath: filePath.replace('/dist/src/', '/dist/'),
-          content
-        };
+export default defineConfig(configEnv => {
+  const viteEnv = loadEnv(configEnv.mode, `.env.${configEnv.mode}`);
+
+  const isVercel = viteEnv.VITE_IS_VERCEL === '1';
+
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    })
-  ],
-  optimizeDeps: {
-    exclude: ['vue-demi']
-  },
-  build: {
-    lib: {
-      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
-      name: 'SoybeanAdminLayout',
-      fileName: 'index'
     },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
+    plugins: [
+      vue(),
+      windicss(),
+      dts({
+        include: ['./src/index.ts', './src/index.vue'],
+        beforeWriteFile(filePath, content) {
+          return {
+            filePath: filePath.replace('/dist/src/', '/dist/'),
+            content
+          };
         }
-      }
-    }
-  }
+      })
+    ],
+    optimizeDeps: {
+      exclude: ['vue-demi']
+    },
+    build: isVercel
+      ? {}
+      : {
+          lib: {
+            entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+            name: 'SoybeanAdminLayout',
+            fileName: 'index'
+          },
+          rollupOptions: {
+            external: ['vue'],
+            output: {
+              globals: {
+                vue: 'Vue'
+              }
+            }
+          }
+        }
+  };
 });
